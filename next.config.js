8ea45b99d72eff2aa865964cf773bf9eb9052d78
tiina-cyber <tiina.m.nyman@gmail.com>
2025-08-30 16:44:01 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        // apply to all routes
+        source: "/(.*)",
+        headers: [
+          // allow Notion to iframe your site
+          { key: "Content-Security-Policy", value: "frame-ancestors https://www.notion.so https://notion.so https://*.notion.site;" },
+          // make sure no default SAMEORIGIN blocks iframing
+          { key: "X-Frame-Options", value: "ALLOWALL" }
+        ],
+      },
+    ];
+  },
+};
 module.exports = nextConfig;
