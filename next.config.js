5027ddf6e123b392d203021efc6a58c31496b4d2
tiina-cyber <tiina.m.nyman@gmail.com>
2025-08-30 16:48:00 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,13 +3,13 @@ const nextConfig = {
   async headers() {
     return [
       {
-        // apply to all routes
         source: "/(.*)",
         headers: [
           // allow Notion to iframe your site
           { key: "Content-Security-Policy", value: "frame-ancestors https://www.notion.so https://notion.so https://*.notion.site;" },
-          // make sure no default SAMEORIGIN blocks iframing
-          { key: "X-Frame-Options", value: "ALLOWALL" }
+          { key: "X-Frame-Options", value: "ALLOWALL" },
+          // prevent search engines from indexing
+          { key: "X-Robots-Tag", value: "noindex, nofollow" }
         ],
       },
     ];
